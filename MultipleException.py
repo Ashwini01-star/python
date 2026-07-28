61886968da73bf2c1812c43cbc49c91902164a55
Ashwini01-star <khandareashwini947@gmail.com>
2026-07-28 10:46:16 +0530

    Delete MultipleException.py

--- MultipleException.py
+++
@@ -1,9 +0,0 @@
-try:
-    a=int(input(" enter 1st no: "))
-    b= int(input(" enter 2nd no: "))
-
-    print(a/b)
-except ZeroDivisionError as z:
-    print(z)
-except ValueError as v:
-    print(v)
