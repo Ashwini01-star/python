d6bc00837a906a56e9d7d4e29f1f98a5e9dc0966
Ashwini01-star <khandareashwini947@gmail.com>
2026-07-28 10:54:44 +0530

    Delete XO.py

--- XO.py
+++
@@ -1,8 +0,0 @@
-for i in range(3):      #2                          # X O X
-                                                   # O X O
-    for j in range(3):  #2                        # X O X
-          if((i+j) %2==0): 
-            print("X", end = " ")
-          else:
-            print("O",end = " ")
-    print()
