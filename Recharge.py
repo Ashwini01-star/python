89df3d5232bb6ed92a55ff586899e17f686086d2
Ashwini01-star <khandareashwini947@gmail.com>
2026-07-28 10:51:12 +0530

    Delete Recharge.py

--- Recharge.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter number of customers: "))
-
-total = 0
-
-for i in range(n):
-    plan = int(input("Enter recharge plan (199/399/599): "))
-    total += plan
-
-print("Total Earnings =", total)
